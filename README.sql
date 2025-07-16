f7085b0317977b455374db1301b0398c03dc8ad2
Tejasmreddy <30894722+Tejasmreddy@users.noreply.github.com>
2025-07-16 11:06:12 +0530

    README.sql

--- README.sql
+++ README.sql
@@ -1,5 +1,5 @@
 -- Task4 Develop the queries to retrieve information from the OLAP operations performed and to gain a deeper understanding of the sales data through different dimensions, aggregations, and filters.
--- Project: OLAP Operations (using Redshift or PostgreSQL)
+-- Project: OLAP Operations (using Mysql or PostgreSQL)
 -- Objective: Perform OLAP operations (Drill Down, Rollup, Cube, Slice, and Dice) on the"sales_sample" table to analyze sales data. The project will include the following tasks:
 
 -- 1.Database Creation
